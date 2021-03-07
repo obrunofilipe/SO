afcc62e3f42848e728cd33ef57e41827d27cc80f
Bruno Filipe <brunofilipe377@gmail.com>
2021-03-07 19:53:23 +0000

    files remmoved

--- guiao1/script.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash                                                                                                                                                                                       
-for i in {1..100000} 
-  do  
-    echo "test $i" >> test
-  done
