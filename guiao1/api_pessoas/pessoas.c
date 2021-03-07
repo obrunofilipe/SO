bb6654d33bb71d26459968f1f72dc473ef284dd7
Bruno Filipe <brunofilipe377@gmail.com>
2021-03-07 23:01:44 +0000

    5 e 6 atualizado

--- guiao1/api_pessoas/pessoas.c
+++ guiao1/api_pessoas/pessoas.c
@@ -10,20 +10,15 @@ int load_register(int fd, PESSOA* p1){
 }
 void add_Pessoa(int fd ,char *pessoa, char* idade){
     int wb = 0;
-    char b_written[15];
     lseek(fd,0,SEEK_END);
     PESSOA p1 = malloc(sizeof(struct pessoa));
     p1->idade = atoi(idade);
     strcpy(p1->nome ,pessoa);
     wb = write(fd,p1,sizeof(struct pessoa));
-    sprintf(b_written, "bytes: %d\n",wb);
-    write(STDOUT_FILENO,b_written,10);
 }
 
 void update_Age(int fd, char* pessoa, char* idade){
-    int wb = 0,bread = 0;
-    char *nome;
-    char b_read[15];
+    int bread = 0;
     PESSOA p1 = malloc(sizeof(struct pessoa));
     bread = load_register(fd,&p1);
     while(bread > 0 && strcmp(p1->nome, pessoa) != 0){
