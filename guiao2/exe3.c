291769309d333812d789fae0e4fccf9f3a38984b
obrunofilipe <brunofilipe377@gmail.com>
2021-03-18 22:56:26 +0000

    3 atualizado

--- guiao2/exe3.c
+++ guiao2/exe3.c
@@ -4,28 +4,21 @@
 #include <sys/wait.h>
 
 int main(){
-    pid_t pids[10];
-    pid_t tmp_pid, c_pid;
-    int i = 0,r;
+    pid_t ppid, pid;
+    int r;
     // criar processos 10 filhos a partir do processo atual 
     // nao criar processos filhos a partir de processos filhos
-    while( i < 10 && (c_pid = fork()) != 0 ){
-        pids[i] = c_pid;
-        i++;
-    }
-    if(c_pid == 0){
-        printf("Processo filho nº: %d\nPID: %d\nPID(pai): %d",i,getpid(),getppid());
-        _exit(i);
-    }
-    else{
-        i = 0;
-        while(i < 10){
-            tmp_pid = waitpid(pids[i],&r,0);
-            if(WIFEXITED(r)){
-                r = WEXITSTATUS(r);
-                printf("\n:::: Processo %d ::::\n PID: %d \n Saída: %d\n\n",i,pids[i],r);
-            }
-            i++;
+    for(int i = 0 ; i < 10 ; i++){
+        if(fork() == 0){
+            ppid = getppid();
+            pid = getpid();
+            printf(":::: Processo %d ::::\n PID: %d\n PID(pai): %d\n",i,pid,ppid);
+            _exit(i);
+        }
+        wait(&r);
+        if(WIFEXITED(r)){
+            r  = WEXITSTATUS(r);
+            printf(" Saída: %d\n",r);
         }
     }
     return 0;
